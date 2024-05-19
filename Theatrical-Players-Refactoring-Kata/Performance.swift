fed8c69708f557cf27f72823cdc14ad2d31bcc1d
Eric Mentele <ericmentele@gmail.com>
2024-05-19 07:00:25 -0500

    Create `PerformanceCharge` with play name. Migrate play name from `Performance.play`

--- Theatrical-Players-Refactoring-Kata/Performance.swift
+++ Theatrical-Players-Refactoring-Kata/Performance.swift
@@ -2,7 +2,21 @@ struct Performance {
     let playID: String
     let audience: Int
     
+    var charge: PerformanceCharge
     var play: Play?
     var cost: Int?
     var volumeCredits: Int?
+    
+    init(playID: String, audience: Int, charge: PerformanceCharge = .init(playName: "Placeholder"), play: Play? = nil, cost: Int? = nil, volumeCredits: Int? = nil) {
+        self.playID = playID
+        self.audience = audience
+        self.charge = charge
+        self.play = play
+        self.cost = cost
+        self.volumeCredits = volumeCredits
+    }
+}
+
+struct PerformanceCharge {
+    let playName: String
 }
