890d6ff6606d003e18ed8b59ba9a415bdbe0669c
Eric Mentele <ericmentele@gmail.com>
2024-05-19 07:45:48 -0500

    Remove cost from `Performance`

--- Theatrical-Players-Refactoring-Kata/Performance.swift
+++ Theatrical-Players-Refactoring-Kata/Performance.swift
@@ -4,14 +4,17 @@ struct Performance {
     
     var charge: PerformanceCharge
     
-    var cost: Int?
     var volumeCredits: Int?
     
-    init(playID: String, audience: Int, charge: PerformanceCharge = .init(playName: "Placeholder", cost: 0), play: Play? = nil, cost: Int? = nil, volumeCredits: Int? = nil) {
+    init(
+        playID: String,
+        audience: Int,
+        charge: PerformanceCharge = PerformanceCharge(playName: "Placeholder", cost: 0),
+        volumeCredits: Int? = nil
+    ) {
         self.playID = playID
         self.audience = audience
         self.charge = charge
-        self.cost = cost
         self.volumeCredits = volumeCredits
     }
 }
