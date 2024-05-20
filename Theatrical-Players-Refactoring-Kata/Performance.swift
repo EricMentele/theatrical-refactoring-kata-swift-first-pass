887f963acf374654d6dab6ec72b1b344f116dda5
Eric Mentele <ericmentele@gmail.com>
2024-05-20 14:31:04 -0500

    Move `PerformanceCharge` to it's own file

--- Theatrical-Players-Refactoring-Kata/Performance.swift
+++ Theatrical-Players-Refactoring-Kata/Performance.swift
@@ -2,10 +2,3 @@ struct Performance {
     let playID: String
     let audience: Int
 }
-
-struct PerformanceCharge {
-    let playName: String
-    let cost: Int
-    let volumeCredits: Int
-    let attendanceCount: Int
-}
---
+++ Theatrical-Players-Refactoring-Kata/PerformanceCharge.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct PerformanceCharge {
+    let playName: String
+    let cost: Int
+    let volumeCredits: Int
+    let attendanceCount: Int
+}
