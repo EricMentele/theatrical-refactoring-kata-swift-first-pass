937f61a55d127685a55ee3df7befb7474c3fe58e
Eric Mentele <ericmentele@gmail.com>
2024-05-20 17:19:46 -0500

    `StatementPrinter.generateStatementHTML` produces html for known plays

--- Theatrical-Players-Refactoring-Kata/StatementPrinter.swift
+++ Theatrical-Players-Refactoring-Kata/StatementPrinter.swift
@@ -1,6 +1,10 @@
 class StatementPrinter {
     func generateStatement(_ invoice: Invoice, _ plays: Dictionary<String, Play>) throws -> String {
-        return try renderPlainText(generateStatementData(invoice, plays))
+        try renderPlainText(generateStatementData(invoice, plays))
+    }
+    
+    func generateStatementHTML(_ invoice: Invoice, _ plays: Dictionary<String, Play>) throws -> String {
+        try renderHTML(generateStatementData(invoice, plays))
     }
     
     private func renderPlainText(_ data: StatementData) throws -> String {
@@ -14,7 +18,20 @@ class StatementPrinter {
         result += "Amount owed is \(usd(amount: data.totalAmount))\n"
         result += "You earned \(data.totalVolumeCredits) credits\n"
         return result
-        
+    }
+    
+    private func renderHTML(_ data: StatementData) throws -> String {
+        var result = "<h1>Statement for \(data.customer)</h1>\n"
+        result += "<table>\n"
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+        for perf in data.performanceCharges {
+            result += "<tr><td>\(perf.playName)</td><td>\(perf.attendanceCount)</td>"
+            result += "<td>\(usd(amount: perf.cost))</td></tr>\n"
+        }
+        result += "</table>\n"
+        result += "<p>Amount owed is <em>\(usd(amount: data.totalAmount))</em></p>\n"
+        result += "<p>You earned <em>\(data.totalVolumeCredits)</em> credits</p>\n"
+        return result
     }
     
     private func usd(amount: Int) -> String {
